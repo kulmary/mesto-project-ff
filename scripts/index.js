530113ec497f73e3198feb96368b8f99046230e4
Maria Kuleshova <m1800597@edu.misis.ru>
2023-12-05 18:48:06 +0300

    First commit

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,28 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+const places = document.querySelector('.places');
+const placesList = places.querySelector('.places__list');
+
+
+function removeCard(card) {
+    card.remove();
+};
+
+
+function addCard(cardName, cardLink, removeCard) {
+    const cardTemplate = document.querySelector('#card-template').content;
+    const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+    cardElement.querySelector('.card__title').textContent = cardName;
+    cardElement.querySelector('.card__image').src = cardLink;
+    const removeButton = cardElement.querySelector('.card__delete-button');
+    removeButton.addEventListener('click', removeCard(cardElement));
+    return cardElement;
+};
+
+initialCards.forEach(function (element) {
+    placesList.append(addCard(element.name, element.link, removeCard));
+});
+
+
